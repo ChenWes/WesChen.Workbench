f02f74c786db7c2e03fd8108598ae76b3dd439a3
ChenWes <chenxuhua0530@163.com>
2016-11-16 15:28:40 +0800

    project init

---
+++ WesChen.Workbench/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace WesChen.Workbench
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ WesChen.Workbench/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WesChen.Workbench.WebApiApplication" Language="C#" %>
